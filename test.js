9b6f0333bc666bde224c2591b75363e93224e689
Al Joslin <allen.joslin@gmail.com>
2018-01-07 18:24:15 -0500

    update testing script

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 
-const MQTTClient = require('./index');
+const MQTTClient = require('mqtt-client');
 console.log(MQTTClient);
 
 let mqttClient = new MQTTClient();
 console.log(mqttClient);
+
+mqttClient.connect('testBackend'); // blocks until disconnect
+
+mqttClient.subscribe('test/backend', { qos: 2 }, data => {
+    let msg = data.toString();
+    console.info('test/backend', msg);
+    mqttClient.unsubscribe('test/backend');
+    mqttClient.disconnect();
+});
+
+
